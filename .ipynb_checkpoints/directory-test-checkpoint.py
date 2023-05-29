c5eb537b4b48d021af462b4ce7e587c3221ba8d1
Buzz <75030067+sheep52031@users.noreply.github.com>
2023-05-29 19:22:07 +0800

    Update commit

--- .ipynb_checkpoints/directory-test-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
